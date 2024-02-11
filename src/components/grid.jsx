3e1e8e1296ddbd374ebdd69e5319e7373076c13c
Adhar Singh <130926943+MciSA2023@users.noreply.github.com>
2024-02-11 12:30:58 +0100

    Grid größe angepasst

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -19,28 +19,28 @@ function Grid () {
             <div className="col">10</div>
         </div>
         <div className="row">
-        <div className="col">11</div>
-        <div className="col">12</div>
-        <div className="col">13</div>
-        <div className="col">14</div>
-        <div className="col">15</div>
-           
-        </div>
-        <div className="row">
-        <div className="col">16</div>
-        <div className="col">17</div>
-        <div className="col">18</div>
-        <div className="col">19</div>
-        <div className="col">20</div>
+            <div className="col">11</div>
+            <div className="col">12</div>
+            <div className="col">13</div>
+            <div className="col">14</div>
+            <div className="col">15</div>
             
         </div>
         <div className="row">
-        <div className="col">21</div>
-        <div className="col">22</div>
-        <div className="col">23</div>
-        <div className="col">24</div>
-        <div className="col">25</div>
+            <div className="col">16</div>
+            <div className="col">17</div>
+            <div className="col">18</div>
+            <div className="col">19</div>
+            <div className="col">20</div>
+                
         </div>
+        <div className="row">
+            <div className="col">21</div>
+            <div className="col">22</div>
+            <div className="col">23</div>
+            <div className="col">24</div>
+            <div className="col">25</div>
+            </div>
     </div>
   );
 }
--- src/screens/mainscreen.jsx
+++ src/screens/mainscreen.jsx
@@ -4,8 +4,6 @@ import Grid from '../components/grid';
 function Mainscreen() {
   return (
     <div>
-        <h1>My First React App</h1>
-        <p>Welcome to my first React app. This is the main screen.</p>
         <Grid />
     </div>
   );
