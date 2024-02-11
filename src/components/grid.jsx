d4b87382a7ca7e53cf51a7fddbcc2fcdf5b67ecf
Adhar Singh <130926943+MciSA2023@users.noreply.github.com>
2024-02-11 12:24:24 +0100

    Grid 5x5

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -8,16 +8,38 @@ function Grid () {
             <div className="col">1</div>
             <div className="col">2</div>
             <div className="col">3</div>
-        </div>
-        <div className="row">
             <div className="col">4</div>
             <div className="col">5</div>
-            <div className="col">6</div>
         </div>
         <div className="row">
+            <div className="col">6</div>
             <div className="col">7</div>
             <div className="col">8</div>
             <div className="col">9</div>
+            <div className="col">10</div>
+        </div>
+        <div className="row">
+        <div className="col">11</div>
+        <div className="col">12</div>
+        <div className="col">13</div>
+        <div className="col">14</div>
+        <div className="col">15</div>
+           
+        </div>
+        <div className="row">
+        <div className="col">16</div>
+        <div className="col">17</div>
+        <div className="col">18</div>
+        <div className="col">19</div>
+        <div className="col">20</div>
+            
+        </div>
+        <div className="row">
+        <div className="col">21</div>
+        <div className="col">22</div>
+        <div className="col">23</div>
+        <div className="col">24</div>
+        <div className="col">25</div>
         </div>
     </div>
   );
