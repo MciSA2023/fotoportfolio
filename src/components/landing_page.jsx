2732366dca0e175d057d73e22ac18cadfcc40fdd
Adhar Singh <130926943+MciSA2023@users.noreply.github.com>
2024-02-28 22:53:35 +0100

    Landing Page mit (Navbar)

--- src/components/landing_page.jsx
+++ src/components/landing_page.jsx
@@ -5,8 +5,18 @@ import slides from './carouselData.json'; // Directly import the array of slides
 function Landing_page() {
     return (
         <div className='landing_page_main_container'>
-            <div>
-                <Carosell data={slides} /> {/* Pass slides directly */}
+            <div className='landing_page_navbar'>
+                <div className='landing_page_navbar_links'>
+                    <ul className='nav_items'>
+                        <li ><a href='#' className='nav_item'>Home</a></li>
+                        <li ><a href='#' className='nav_item'>Photos</a></li>
+                        <li ><a href='#' className='nav_item'>Gear</a></li>
+                        <li ><a href='#' className='nav_item'>Contact</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <Carosell data={slides} />
+                </div>
             </div>
         </div>
     );
