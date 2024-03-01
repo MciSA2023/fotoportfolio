81d048e5ef89cfcae08d8c87449e38f18ced4cd4
Adhar Singh <130926943+MciSA2023@users.noreply.github.com>
2024-03-01 21:28:21 +0100

    layout for better usebility on phone

--- src/components/landing_page.jsx
+++ src/components/landing_page.jsx
@@ -5,19 +5,21 @@ import slides from './carouselData.json'; // Directly import the array of slides
 function Landing_page() {
     return (
         <div className='landing_page_main_container'>
-            <div className='landing_page_navbar'>
-                <div className='landing_page_navbar_links'>
-                    <ul className='nav_items'>
-                        <li ><a href='#' className='nav_item'>Home</a></li>
-                        <li ><a href='#' className='nav_item'>Photos</a></li>
-                        <li ><a href='#' className='nav_item'>Gear</a></li>
-                        <li ><a href='#' className='nav_item'>Contact</a></li>
-                    </ul>
-                </div>
+            <div>
+                <div className='landing_page_navbar'>
+                    <div className='landing_page_navbar_links'>
+                        <ul className='nav_items'>
+                            <li className='nav_item_container' ><a href='#' className='nav_item'>Home</a></li>
+                            <li className='nav_item_container'><a href='#' className='nav_item'>Photos</a></li>
+                            <li className='nav_item_container'><a href='#' className='nav_item'>Gear</a></li>
+                            <li className='nav_item_container'><a href='#' className='nav_item'>Contact</a></li>
+                        </ul>
+                    </div>
                 <div>
-                    <Carosell data={slides} />
-                </div>
             </div>
+        </div>
+            <Carosell data={slides} />
+            </div>     
         </div>
     );
 }
