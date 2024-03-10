40e65024318f73b6719544094418da58d1dccd5f
Adhar Singh <130926943+MciSA2023@users.noreply.github.com>
2024-03-10 10:21:01 +0100

    Carosell Bilder hinzugefügt

--- src/components/undercomponents/carosell.jsx
+++ src/components/undercomponents/carosell.jsx
@@ -1,38 +1,40 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs';
 import './carosell.css';
 
 const Carosell = ({ data }) => {
-    const [slide, setslide] = useState(0);
+    const [slide, setSlide] = useState(0);
 
     const nextSlide = () => {
-        setslide(slide === data.length - 1 ? 0 : slide + 1);
+        setSlide(slide === data.length - 1 ? 0 : slide + 1);
     }
 
     const prevSlide = () => {
-        setslide(slide === 0 ? data.length - 1 : slide - 1);
+        setSlide(slide === 0 ? data.length - 1 : slide - 1);
     }
-    
+
     // Check if data is not an array
     if (!Array.isArray(data)) {
         console.error('Data is not an array:', data);
         return null; // Or return a message or UI indicating the error
     }
 
+    const imgPath = (index) => require(`../../img/DSC${index}.JPG`);
+
     return (
         <div className='carosell_main'>
-            <BsArrowLeftCircleFill className='arrow arrow_left' onClick={prevSlide}/>
+            <BsArrowLeftCircleFill className='arrow arrow_left' onClick={prevSlide} />
             {data.map((item, index) => {
                 return (
-                <img src={item.src} alt={item.alt} key={index} className={slide === index ? "slide": "slide slide-hidden"}/>
+                    <img src={imgPath(item.src)} alt={item.alt} key={index} className={slide === index ? "slide" : "slide slide-hidden"} />
                 );
             })}
             <BsArrowRightCircleFill className='arrow arrow_right' onClick={nextSlide} />
             <span className='indicators'>
                 {data.map((_, index) => {
                     return (
-                    <button key={index} onClick={()=>setslide(index)} className={slide === index ? "indicator":"indicator indicator_inactive"}></button>
+                        <button key={index} onClick={() => setSlide(index)} className={slide === index ? "indicator" : "indicator indicator_inactive"}></button>
                     );
                 })}
             </span>
