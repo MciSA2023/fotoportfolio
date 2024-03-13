d5f530dee531f72ffa72b6aafae4f138195efc5d
Adhar Singh <130926943+MciSA2023@users.noreply.github.com>
2024-03-13 20:30:32 +0100

    refactoring

--- src/components/undercomponents/foto_gallery.jsx
+++ src/components/undercomponents/foto_gallery.jsx
@@ -3,8 +3,8 @@ import pictures from '../../services/getpictures';
 import './foto_gallery.css';
 
 const FotoGallery = () => {
-    const [images, setImages] = useState(Array.from({ length: 3 }, (_, i) => i + 1));
-    const [loadedImages, setLoadedImages] = useState(3);
+    const [images, setImages] = useState(Array.from({ length: 1 }, (_, i) => i + 1));
+    const [loadedImages, setLoadedImages] = useState(1);
 
     const loadMoreImages = () => {
         const newImages = Array.from({ length: loadedImages + 3 }, (_, i) => i + 1);
@@ -16,21 +16,32 @@ const FotoGallery = () => {
         const image = e.target;
         image.requestFullscreen();
     }
-    
 
     return (
         <div className="foto-gallery">
             <div className="grid-container">
-                {pictures.map((image, index) => (
-                    <div className="grid-item" key={index}onClick={openfullscreen}>
-                        <img src={image.imagePath} alt={image.category} />
-                    </div>
-                ))}
+                {pictures.map((image, index) => {
+                    if (image.format === 'portrait') {
+                        return (
+                            <div className="grid-item portrait-size" key={index} onClick={openfullscreen}>
+                                <img src={image.imagePath} alt={image.category} />
+                            </div>
+                        );
+                    }
+                    if (image.format === 'landscape') {
+                        return (
+                            <div className="grid-item landscape-size" key={index} onClick={openfullscreen}>
+                                <img src={image.imagePath} alt={image.category} />
+                            </div>
+                        );
+                    }
+                    return null;
+                })}
             </div>
             {loadedImages < 100 && (
-            <div className=''>
-                <button onClick={loadMoreImages}>Load More</button>
-            </div>
+                <div className=''>
+                    <button onClick={loadMoreImages}>Load More</button>
+                </div>
             )}
         </div>
     );
